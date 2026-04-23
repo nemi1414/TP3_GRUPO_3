e34efd0dda66be8bd9a90f3d5f816eaa09f02311
LucasBerlingeri <lucasberlingeri4@gmail.com>
2026-04-23 09:17:00 -0300

    Agrego baja y listado de categorias

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -1,8 +1,12 @@
 package dao;
-
+ 
+import entidad.Categoria;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import entidad.Categoria;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DaoCategoria {
 
@@ -39,4 +43,40 @@ public class DaoCategoria {
 		}
 		return 0;
 	}
+	
+	 public int eliminarCategoria(int idCategoria) {
+	        String query = "DELETE FROM Categorias WHERE IdCategoria = ?";
+	        Conexion conexion = new Conexion();
+	        Connection cn = null;
+	        try {
+	            cn = conexion.getConnection();
+	            PreparedStatement ps = cn.prepareStatement(query);
+	            ps.setInt(1, idCategoria);
+	            return ps.executeUpdate();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return 0;
+	    }
+	 
+	    public List<Categoria> listarCategorias() {
+	        List<Categoria> lista = new ArrayList<>();
+	        String query = "SELECT IdCategoria, Nombre FROM Categorias";
+	        Conexion conexion = new Conexion();
+	        Connection cn = null;
+	        try {
+	            cn = conexion.getConnection();
+	            Statement st = cn.createStatement();
+	            ResultSet rs = st.executeQuery(query);
+	            while (rs.next()) {
+	                Categoria c = new Categoria();
+	                c.setIdCategoria(rs.getInt("IdCategoria"));
+	                c.setNombre(rs.getString("Nombre"));
+	                lista.add(c);
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return lista;
+	    }
 }
