3a9652602e411048442df92b6524ee2c8cab5fb6
Matias Zarate <m.zarate92@hotmail.com>
2026-04-22 22:52:35 -0300

    Clase Produccion

---
+++ src/entidad/Producto.java
@@ -0,0 +1,61 @@
+package entidad;
+
+public class Producto {
+
+	private String codigo;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private int idCategoria;
+	
+	public Producto() {}
+	
+	public Producto(String codigo, String nombre, double precio, int stock, int idCategoria) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.idCategoria = idCategoria;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", nombre=" + nombre + ", precio=" + precio + ", stock=" + stock
+				+ ", idCategoria=" + idCategoria + "]";
+	}
+	
+	
+	
+}
